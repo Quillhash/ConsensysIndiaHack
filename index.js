06bdad580db2764c6f7e125acf14cd652cffddbe
himanshuchawla009 <himanshuchawla2014@gmail.com>
2018-07-13 15:25:33 +0530

    port fixed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require('path')
-
+var port= 3000 || process.env.port
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname +'/index.html'));
 
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("listening");
 })
